838cff2e1130165742ff4c102644bf94461a52d2
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-02-19 17:22:42 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,3 @@
 explorer https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_windows-x64_bin.zip
-
+cd 7-Zip
+7z e %appdata%\..\..\Downloads\openjdk-15.0.2_windows-x64_bin.zip
